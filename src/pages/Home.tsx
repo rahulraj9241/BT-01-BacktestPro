4c48a09c8326c21753cf75ab7345efe8de12d813
rahulraj9241 <rahulraj9241516164@gmail.com>
2026-07-03 15:09:11 +0000

    Create Home page and project structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+      return (
+          <div>
+                <h1>📈 BacktestPro</h1>
+                      <p>Professional Forex & XAUUSD Backtesting Software</p>
+                          </div>
+                            );
+                            }
+
+                            export default Home;
+
